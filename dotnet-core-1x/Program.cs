9d42a972df04e6b4a7c453f4e150db17ad8a4afd
aj.jammy <aj.jammy09@gmail.com>
2017-11-30 00:17:24 +0700

    Create dotnet core 1.x

--- dotnet-core-1x/Program.cs
+++ dotnet-core-1x/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace yoweb_core1
+namespace dotnet_core_1x
 {
     public class Program
     {
--- dotnet-core-1x/Startup.cs
+++ dotnet-core-1x/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace yoweb_core1
+namespace dotnet_core_1x
 {
     public class Startup
     {
